a39707e555da2aa5ba0137679378cd794a8bbd5b
Dpy3b <57526052+Dpy3b@users.noreply.github.com>
2022-05-03 18:18:36 +0300

    practice redux

---
+++ src/store/reducers/index.ts
@@ -0,0 +1,12 @@
+import { todoReducer } from './todoReducer';
+import { userReducer } from './userReducer';
+import { combineReducers, Dispatch } from 'redux';
+
+export const rootReducer = combineReducers({
+	user: userReducer,
+	todo: todoReducer,
+});
+
+
+// получаем тип нашего редюсера с помощью тайпскриптовского ReturnType
+export type RootState = ReturnType<typeof rootReducer>;
